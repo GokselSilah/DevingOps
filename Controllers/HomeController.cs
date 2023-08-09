71c946e7e9b54e9b9f5cbb572f9ddf29cd30c7b9
GokselSilah <85080540+GokselSilah@users.noreply.github.com>
2023-08-09 13:51:36 +0300

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace DevingOps.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionReult Index()
         {
             return View();
         }
